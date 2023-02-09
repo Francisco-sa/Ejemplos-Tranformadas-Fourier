1e15bb6f5356cfeac853907e1450bed08de31222
Francisco Santos <francsantos17@gmail.com>
2023-02-08 20:35:22 -0600

    Initial commit

---
+++ Transformada_Rapida.m
@@ -0,0 +1,16 @@
+%serie temporal
+n=2^14;
+dt=0.4;
+t=(0:n-1)*dt; %vector de tiempos
+x=cos(t)+0.5*cos(3*t)+0.4*cos(3.5*t)+0.7*cos(4*t)+0.2*cos(6*t);
+
+%amplitud-fase vs. frecuencias
+g=fft(x);
+power=abs(g).^2;
+dw=2*pi/(n*dt);
+w=(0:n-1)*dw; %vector de frecuencias angulares
+
+plot(w,power)
+xlabel('\omega')
+ylabel('P(\omega)')
+title('Espectro de potencia')
